41a81b0214cd8b7619f081c8dea7cef3a8d4f027
3real <mikegrahameverett@gmail.com>
2017-01-03 01:16:04 +0000

    Initial commit
    Added initial project

---
+++ testcardboard5(/Assets/controls2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls2 : MonoBehaviour {
+
+    public bool walking = false;
+    private Vector3 spawnPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (walking)
+        {
+
+            transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
+
+        }
+
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name.Contains("plane"))
+            {
+                walking = false;
+            }
+            else
+            {
+                walking = true;
+            }
+        }
+
+    }
+}
